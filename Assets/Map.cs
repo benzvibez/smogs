f292b80ebf56367a9e257a7c9cc8e723b012be4e
benzvibez <bendunbar123@gmail.com>
2022-11-30 14:24:16 -0600

    hey kankan
    ;)

---
+++ Assets/Map.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+
+    Image image;
+
+    
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+   
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            image.enabled = !image.enabled;
+        }
+    }
+}
