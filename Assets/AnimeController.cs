1be170e5d418c42c66ee0234ed3806d58b1cbd5b
benzvibez <80062871+benzvibez@users.noreply.github.com>
2022-12-19 14:35:00 -0600

    NIGER

---
+++ Assets/AnimeController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+
+
+public class AnimeController : MonoBehaviour
+{
+    public Animator animeController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animeController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var idle = animeController.GetBool("idle");
+        var walking = animeController.GetBool("walking");
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (idle && !walking)
+            {
+                animeController.SetBool("walking", true);
+                animeController.SetBool("idle", false);
+            }
+            else
+            {
+                animeController.SetBool("walking", false);
+                animeController.SetBool("idle", true);
+
+            }
+        }
+    }
+}
